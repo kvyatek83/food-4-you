8e23cacb45824ff0277e3e5a9f3fcba3bc5c7839
kvyatek83 <kvyatek83@gmail.com>
2025-01-22 20:47:00 +0200

    category and item component

--- src/app/travler/category/category.component.ts
+++ src/app/travler/category/category.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Category } from '../travler.model';
+import { CommonModule } from '@angular/common';
+import { ItemComponent } from '../item/item.component';
+import { PropertiesTranslationPipe } from '../../pipes/properties-translation.pipe';
+import { Observable } from 'rxjs';
+import { LanguageService, LanguageType } from '../../services/lang.service';
 
 @Component({
   selector: 'app-category',
-  imports: [],
+  imports: [CommonModule, ItemComponent, PropertiesTranslationPipe],
   templateUrl: './category.component.html',
-  styleUrl: './category.component.scss'
+  styleUrl: './category.component.scss',
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit {
+  @Input() category: Category | undefined;
+  public lang$: Observable<LanguageType> = new Observable<LanguageType>();
 
+  constructor(private languageService: LanguageService) {}
+
+  ngOnInit(): void {
+    this.lang$ = this.languageService.currentLanguage$;
+  }
 }
--- src/app/travler/item/item.component.ts
+++ src/app/travler/item/item.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Item } from '../travler.model';
+import { Observable } from 'rxjs';
+import { LanguageService, LanguageType } from '../../services/lang.service';
+import { CommonModule } from '@angular/common';
+import { PropertiesTranslationPipe } from '../../pipes/properties-translation.pipe';
+import { MaterialModule } from '../../material.module';
+import { LanguageDirectionDirective } from '../../directives/language-direction.directive';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-item',
-  imports: [],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    LanguageDirectionDirective,
+    PropertiesTranslationPipe,
+  ],
   templateUrl: './item.component.html',
-  styleUrl: './item.component.scss'
+  styleUrl: './item.component.scss',
 })
-export class ItemComponent {
+export class ItemComponent implements OnInit {
+  @Input() item: Item | undefined;
 
+  public lang$: Observable<LanguageType> | undefined;
+
+  constructor(
+    private languageService: LanguageService,
+    private cartService: CartService
+  ) {}
+
+  ngOnInit(): void {
+    this.lang$ = this.languageService.currentLanguage$;
+  }
+
+  addItemToCart(item?: Item): void {
+    // TODO: for complex item need popup for selection
+    if (item) {
+      this.cartService.addItem(item);
+    }
+  }
 }
