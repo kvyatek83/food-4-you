2bb5bf2d9124918554239820724d8a8447f662d7
kvyatek83 <kvyatek83@gmail.com>
2025-01-29 12:27:25 +0200

    menu is ready

--- src/app/travler/menu/menu.component.ts
+++ src/app/travler/menu/menu.component.ts
@@ -1,16 +1,82 @@
-import { Component } from '@angular/core';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { CategoryComponent } from '../category/category.component';
+import { Category } from '../travler.model';
+import { CommonModule } from '@angular/common';
+import { LanguageDirectionDirective } from '../../directives/language-direction.directive';
+import { PropertiesTranslationPipe } from '../../pipes/properties-translation.pipe';
+import { Observable } from 'rxjs';
+import { LanguageService, LanguageType } from '../../services/lang.service';
+import { ItemsService } from '../../services/items.service';
 
 @Component({
   selector: 'app-menu',
-  imports: [TranslateModule],
+  imports: [
+    CommonModule,
+    LanguageDirectionDirective,
+    PropertiesTranslationPipe,
+    TranslateModule,
+    CategoryComponent,
+  ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
 })
-export class MenuComponent {
-  constructor(private translate: TranslateService) {}
+export class MenuComponent implements AfterViewInit {
+  @ViewChild('menuWrapper') menuWrapper!: ElementRef<HTMLDivElement>;
+  @ViewChild('topMenu') topMenu!: ElementRef<HTMLDivElement>;
+  @ViewChild('bottomMenu') bottomMenu!: ElementRef<HTMLDivElement>;
 
-  changeLanguage(lang: string) {
-    this.translate.use(lang);
+  lang$: Observable<LanguageType> = new Observable<LanguageType>();
+
+  categories: Category[] = [];
+  isTopMenuNotInView: boolean = false;
+  isBottomMenuNotInView: boolean = false;
+
+  constructor(
+    private itemsService: ItemsService,
+    private languageService: LanguageService
+  ) {
+    this.categories = this.itemsService.allItems;
+  }
+
+  ngOnInit(): void {
+    this.lang$ = this.languageService.currentLanguage$;
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.checkMenuVisibility();
+    }, 100);
+  }
+
+  onScroll(_: Event) {
+    this.checkMenuVisibility();
+  }
+
+  scrollToCategory(categoryUuid: string): void {
+    const element = document.getElementById(categoryUuid);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+      element.classList.add('heartbeat');
+      setTimeout(() => {
+        element.classList.remove('heartbeat');
+      }, 2500);
+    }
+  }
+
+  private checkMenuVisibility() {
+    if (!this.menuWrapper || !this.topMenu || !this.bottomMenu) return;
+
+    const wrapperRect = this.menuWrapper.nativeElement.getBoundingClientRect();
+    const topMenuRect = this.topMenu.nativeElement.getBoundingClientRect();
+    const bottomMenuRect =
+      this.bottomMenu.nativeElement.getBoundingClientRect();
+
+    this.isTopMenuNotInView =
+      topMenuRect.bottom < wrapperRect.top || topMenuRect.top < wrapperRect.top;
+
+    this.isBottomMenuNotInView =
+      bottomMenuRect.top > wrapperRect.bottom ||
+      bottomMenuRect.bottom > wrapperRect.bottom;
   }
 }
